1cc76b1f21d7b1ff40842cee78ba5e541a27f812
jptorom <101909873+jptorom@users.noreply.github.com>
2022-06-04 13:19:24 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -40,9 +40,9 @@ if __name__ == '__main__':
                 # con la misma clave se imprime el resultado en
                 # el flujo de salida
                 #
-                sys.stdout.write("{}\t{}\n".format(curkey, total))
+                sys.stdout.write("{},{}\n".format(curkey, total))
 
             curkey = key
             total = val
 
-    sys.stdout.write("{}\t{}\n".format(curkey, total))
+    sys.stdout.write("{},{}\n".format(curkey, total))
