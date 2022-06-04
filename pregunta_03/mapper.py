3721fc4afd010d1652e5acb753d9086c99cc0b52
jptorom <101909873+jptorom@users.noreply.github.com>
2022-06-03 20:36:17 -0500

    Update mapper.py

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        beta=line.split(",")[0]
+        alfa=line.split(",")[1]
+        sys.stdout.write("{}\t{}\n".format(beta,alfa))
