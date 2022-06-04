6f61fddc56b864aa9d3d1d2b62343ccd29db5bfd
jptorom <101909873+jptorom@users.noreply.github.com>
2022-06-04 13:17:48 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+        sys.stdout.write("{}\t1\n".format(line.split()[0]))
