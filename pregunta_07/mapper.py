4e8e687d5a98c7d446b92d54c3b167b795cb09bf
jptorom <101909873+jptorom@users.noreply.github.com>
2022-06-05 14:03:18 -0500

    Update mapper.py

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+        sys.stdout.write("{}\t{}\t{}\n".format(line.split()[0],line.split()[1],line.split()[2]))
