066f769ba273156b6c38abb26aaa479f998396ba
jptorom <101909873+jptorom@users.noreply.github.com>
2022-06-05 19:16:44 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,16 @@
+import sys
+
 #
-# >>> Escriba el codigo del reducer a partir de este punto <<<
+# Esta funcion reduce los elementos que tienen la misma clave
 #
+if __name__ == '__main__':
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        numero,letra =line.split()
+        letrica = letra.split(",")
+        for i in letrica:
+            sys.stdout.write("{}\t{}\n".format(i, numero))#
