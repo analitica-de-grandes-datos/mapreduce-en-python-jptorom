99c849cac82e1145dc7606a7daa68c5b18bd339b
jptorom <101909873+jptorom@users.noreply.github.com>
2022-06-07 17:50:02 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -3,5 +3,14 @@
 #
 import sys
 if __name__ == "__main__":
-            for line in sys.stdin:
-                sys.stdout.write("{}\t{}\n".format(line.split()[0],line.split()[1]))
+   
+    for line in sys.stdin:
+
+        par=[]
+        line=line.replace('\n','')
+        num,letras = line.split('\t')
+        letras=letras.split(',')
+        for letra in letras:
+            par.append([num,letra])
+        for par in par:
+            sys.stdout.write(str(par[1])+'\t'+str(par[0])+'\n')
